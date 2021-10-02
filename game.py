65867f7bf7b2b45a76a07289455d4d8188e72919
Юриков Василий <loney2ears@yandex.ru>
2021-10-02 11:56:26 +1000

    game.py: renamed drone class; yurikov.py: changed the logic of the method _get_my_asteroid

--- game.py
+++ game.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from astrobox.space_field import SpaceField
-# TODO - Переименуйте класс своего дрона по шаблону [Фамилия]Drone
-from yurikov import CollectorXXI
 
+from yurikov import YurikovDrone
 
 if __name__ == '__main__':
     scene = SpaceField(
         speed=3,
         asteroids_count=5,
     )
-    d = CollectorXXI()
+    d = YurikovDrone()
     scene.go()
 
--- yurikov.py
+++ yurikov.py
@@ -1,56 +1,39 @@
 from astrobox.core import Drone
 
-
-class WrongDistanceOperandError(BaseException):
-
-    def __init__(self, op):
-        self.op = op
-
-    def __str__(self):
-        return f'Wrong value of "distance" argument: "{self.op}". Must be in ["far", "near"].'
+FAR = max
+NEAR = min
 
 
-class CollectorXXI(Drone):
+class YurikovDrone(Drone):
 
-    def __init__(self):
-        # TODO - Неплохо бы проинспектировать используемый метод в супер-классе. там кварги в параметрах
-        super().__init__()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.my_team = []
 
     def on_born(self):
-        self.target = self._get_my_asteroid(distance='far')
+        self.target = self._get_my_asteroid(mode_func=FAR)
         self.move_at(self.target)
         self.my_team.append(self)
 
-    def _get_my_asteroid(self, distance):
-        # TODO - Нейминг! distance - дистанция. Как другой прораммист должен догадаться,
-        #  что этот параметр - строковые литералы принимает? Это скорее MODE какой-нибудь.
-        #  А возможные варианты вынесите в константы.
+    def _get_my_asteroid(self, mode_func):
+        res_target = (0, self.my_mothership)
+        curr_asteroid_distance_list = []
 
-        # TODO - Алгоритм получился запутанным.
-        #  Сделайте две ветки алгоритма. Общую часть кода вынесите после обоих веток
-        #  Код должен читаться как книга, а не как ребус
-        if distance == 'far':
-            target_asteroid = [0, self.my_mothership]
-            operand = '>'
-        elif distance == 'near':
-            operand = '<'
-            target_asteroid = [10000, self.my_mothership]
-        else:
-            raise WrongDistanceOperandError(op=distance)
-        for asteroid in self.asteroids:
-            if not asteroid.is_empty:
-                # TODO - Старайтесь не использовать eval
-                if eval(f'self.distance_to(asteroid) {operand} target_asteroid[0]'):
-                    target_asteroid = [self.distance_to(asteroid), asteroid]
-        return target_asteroid[1]
+        for astr in self.asteroids:
+            if not astr.is_empty:
+                curr_asteroid_distance_list.append((self.distance_to(astr), astr))
+
+        if curr_asteroid_distance_list:
+            res_target = mode_func(curr_asteroid_distance_list)
+
+        return res_target[1]
 
     def on_stop_at_asteroid(self, asteroid):
         self.load_from(asteroid)
 
     def on_load_complete(self):
         if not self.is_full:
-            self.target = self._get_my_asteroid(distance='near')
+            self.target = self._get_my_asteroid(mode_func=NEAR)
             self.move_at(self.target)
         else:
             self.move_at(self.my_mothership)
@@ -60,5 +43,5 @@ class CollectorXXI(Drone):
 
     def on_unload_complete(self):
         if self.target.is_empty:
-            self.target = self._get_my_asteroid(distance='far')
+            self.target = self._get_my_asteroid(mode_func=FAR)
         self.move_at(self.target)
