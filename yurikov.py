32633c09109af39300c259bc64d6ec58c9b26b01
Юриков Василий <loney2ears@yandex.ru>
2021-10-06 16:45:40 +1000

    Added docstrings + minor fix

--- yurikov.py
+++ yurikov.py
@@ -7,6 +7,10 @@ UNLOAD = 'unload'
 
 
 class SpaceLogger:
+    """
+    Класс для логгера статистики.
+
+    """
 
     def __init__(self, data):
         self.logger = configure_logger()
@@ -21,6 +25,11 @@ class SpaceLogger:
 
 
 class YurikovDrone(Drone):
+    """
+    Класс дрона.
+    Собирает ресурс с астероидов и выгружает его на базе.
+
+    """
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -47,12 +56,40 @@ class YurikovDrone(Drone):
         super().move_at(*args, **kwargs)
 
     def on_born(self):
+        """
+        Вызывается при "рождении" дрона.
+        Формирует список кортежей (<объект_астероида>, <расстояние_до_астероида>)
+        и сортирует его по убыванию.
+        Получение астероида в качестве цели для загрузки-выгрузки
+        будет происходить через обращение к данному списку.
+
+        :return: None
+        """
+
         self.asteroids_with_distance = [(astr, self.my_mothership.distance_to(astr)) for astr in self.asteroids]
         self.asteroids_with_distance.sort(key=lambda k: k[1], reverse=True)
         self.target = self.asteroids_with_distance[0][0]
         self.move_at(self.target)
 
-    def _get_point_to_act(self, action):
+    def _get_target_to_act(self, action):
+        """
+        Получить цель для загрузки-выгрузки ресурса.
+        Проходится по срезу списка asteroids_with_distance с индекса.
+
+        При действии загрузки (action == LOAD), индекс = 0:
+        дрон ищет любой непустой астероид, начиная с самого дальнего от базы.
+
+        При действии выгрузки (action == UNLOAD), индекс = object_index - 1
+        (object_index - индекс объекта, у которого сейчас находится дрон):
+        дрон ищет любой неполный астероид, находящийся ближе к базе (чем текущий объект)
+        по списку asteroids_with_distance.
+
+        Если подходящего астероида для загрузки-выгрузки не нашлось, возвращает объект базы.
+
+        :param action: str, действие, загрузить ресурс с астероида или выгрузить на астероид/базу
+        :return: MotherShip or Asteroid object, объект, до которого полетит дрон
+        """
+
         res_target = self.my_mothership
 
         if action == UNLOAD:
@@ -75,38 +112,74 @@ class YurikovDrone(Drone):
         return res_target
 
     def on_stop_at_asteroid(self, asteroid):
+        """
+        В зависимости от аргумента дрона action, выполняет загрузку или выгрузку ресурса.
+
+        :param asteroid: Asteroid object, объект астероида, у которого остановился дрон
+        :return: None
+        """
+
         if self.action == LOAD:
             self.load_from(asteroid)
         else:
             self.unload_to(asteroid)
 
     def on_load_complete(self):
+        """
+        Вызывается по окончанию загрузки.
+        Если трюм дрона заполнен, меняет action на выгрузку, ищет цель для выгрузки, летит к ней.
+        Иначе, меняет action на загрузку, ищет цель для загрузку, летит к ней.
+
+        :return: None
+        """
+
         if self.is_full:
             self.action = UNLOAD
-            self.target = self._get_point_to_act(action=self.action)
+            self.target = self._get_target_to_act(action=self.action)
             self.move_at(self.target)
         else:
             self.action = LOAD
-            self.target = self._get_point_to_act(action=self.action)
+            self.target = self._get_target_to_act(action=self.action)
             self.move_at(self.target)
 
     def on_stop_at_mothership(self, mothership):
         self.unload_to(mothership)
 
     def on_unload_complete(self):
+        """
+        Вызывается по окончанию выгрузки.
+        Вызывает метод, проверяющий, закончилась ли "миссия" дронов (можно ли логгировать).
+        Если вернулось True:
+            вызывает метод логгирования у менеджера команды (чтобы было единственное логгирование).
+        Если вернулось False:
+            при пустом трюме дрона ищет астероид для загрузки;
+            иначе ищет астероид для выгрузки.
+        Отправляется к цели для загрузки-выгрузки.
+
+        :return: None
+        """
+
         if self._check_for_end():
             if self.manager.need_log:
                 self.manager._log_stats()
         else:
             if self.is_empty:
                 self.action = LOAD
-                self.target = self._get_point_to_act(action=self.action)
+                self.target = self._get_target_to_act(action=self.action)
             else:
                 self.action = UNLOAD
-                self.target = self._get_point_to_act(action=self.action)
+                self.target = self._get_target_to_act(action=self.action)
             self.move_at(self.target)
 
     def _update_stats(self, target):
+        """
+        Вспомогательный метод. Вызывается перед каждом перемещении к цели (метод move_to()).
+        Дополняет словарь менеджера по ключам, в зависимости от заполненности трюма.
+
+        :param target: MotherShip or Asteroid object, объект, до которого полетит дрон
+        :return: None
+        """
+
         if self.is_empty:
             self.manager.stats['empty_range'] += self.distance_to(target)
         elif self.is_full:
@@ -115,12 +188,28 @@ class YurikovDrone(Drone):
             self.manager.stats['not_fully_range'] += self.distance_to(target)
 
     def _check_for_end(self):
+        """
+        Проверяет, закончилась ли "миссия" дронов (можно ли логгировать).
+        Формирует список всех дронов и астероидов.
+        Проверяет, являются ли все объекты списка пустыми
+        (отсутствует ли ресурс в трюмах дронов и недрах астероидов).
+
+        :return: bool, пустые ли все астероиды и все дроны?
+        """
+
         check_list = self.teammates + self.asteroids
         is_empty_for_end = [obj.is_empty for obj in check_list]
         is_empty_for_end.append(self.is_empty)
         return all(is_empty_for_end)
 
     def _log_stats(self):
+        """
+        Инициализация логгера.
+        Метод вызывается лишь раз в конце "миссии" дронов.
+
+        :return: None
+        """
+
         self.need_log = False
         logger = SpaceLogger(data=self.stats)
         logger.log()
