0a4d48449c0713395c88940d6174773bda3e7bb1
Юриков Василий <loney2ears@yandex.ru>
2021-10-09 17:17:46 +1000

    Deleted methods: _get_target_to_act(), on_load_complete(), on_unload_complete(); Updated methods: game_step(), on_stop_at_asteroid() and on_stop_at_mothership()

--- yurikov_drones/yurikov.py
+++ yurikov_drones/yurikov.py
@@ -72,105 +72,28 @@ class YurikovDrone(Drone):
         self.target = self.state_handle.handle()
         self.move_at(self.target)
 
-    def _get_target_to_act(self, action):
-        """
-        Получить цель для загрузки-выгрузки ресурса.
-        Проходится по срезу списка asteroids_with_distance с индекса.
-
-        При действии загрузки (action == LOAD), индекс = 0:
-        дрон ищет любой непустой астероид, начиная с самого дальнего от базы.
-
-        При действии выгрузки (action == UNLOAD), индекс = object_index - 1
-        (object_index - индекс объекта, у которого сейчас находится дрон):
-        дрон ищет любой неполный астероид, находящийся ближе к базе (чем текущий объект)
-        по списку asteroids_with_distance.
-
-        Если подходящего астероида для загрузки-выгрузки не нашлось, возвращает объект базы.
-
-        :param action: str, действие, загрузить ресурс с астероида или выгрузить на астероид/базу
-        :return: MotherShip or Asteroid object, объект, до которого полетит дрон
-        """
-
-        res_target = self.my_mothership
-
-        if action == UNLOAD:
-            loc_distance = self.my_mothership.distance_to(self.target)
-            index = self.asteroids_with_distance.index((self.target, loc_distance)) + 1
-        else:
-            index = 0
-
-        for astrd_with_dist in self.asteroids_with_distance[index:]:
-            astrd = astrd_with_dist[0]
-            if action == LOAD:
-                if not astrd.is_empty:
-                    res_target = astrd
-                    break
-            if action == UNLOAD:
-                if not astrd.is_full:
-                    res_target = astrd
-                    break
-
-        return res_target
-
-    def on_stop_at_asteroid(self, asteroid):
-        """
-        В зависимости от аргумента дрона action, выполняет загрузку или выгрузку ресурса.
-
-        :param asteroid: Asteroid object, объект астероида, у которого остановился дрон
-        :return: None
-        """
-
-        if self.action == LOAD:
-            self.load_from(asteroid)
-        else:
-            self.unload_to(asteroid)
-
-    def on_load_complete(self):
-        """
-        Вызывается по окончанию загрузки.
-        Если трюм дрона заполнен, меняет action на выгрузку, ищет цель для выгрузки, летит к ней.
-        Иначе, меняет action на загрузку, ищет цель для загрузку, летит к ней.
-
-        :return: None
-        """
-
-        if self.is_full:
-            self.action = UNLOAD
-            self.target = self._get_target_to_act(action=self.action)
-            self.move_at(self.target)
-        else:
-            self.action = LOAD
-            self.target = self._get_target_to_act(action=self.action)
-            self.move_at(self.target)
-
-    def on_stop_at_mothership(self, mothership):
-        self.unload_to(mothership)
-
-    def on_unload_complete(self):
-        """
-        Вызывается по окончанию выгрузки.
-        Вызывает метод, проверяющий, закончилась ли "миссия" дронов (можно ли логгировать).
-        Если вернулось True:
-            вызывает метод логгирования у менеджера команды (чтобы было единственное логгирование).
-        Если вернулось False:
-            при пустом трюме дрона ищет астероид для загрузки;
-            иначе ищет астероид для выгрузки.
-        Отправляется к цели для загрузки-выгрузки.
-
-        :return: None
-        """
-
+    def game_step(self):
         if self._check_for_end():
             if self.manager.need_log:
                 self.manager.log_stats()
-        else:
-            if self.is_empty:
-                self.action = LOAD
-                self.target = self._get_target_to_act(action=self.action)
-            else:
-                self.action = UNLOAD
-                self.target = self._get_target_to_act(action=self.action)
-            self.move_at(self.target)
+
+        self._update_stats(point=self.prev_point)
+
+        self.prev_point = Point(self.coord.x, self.coord.y)
+
+        self.state_handle.on_game_step()
+
+        super().game_step()
+
+    def on_stop_at_asteroid(self, asteroid):
+        self.state_handle = self.state_handle.next_state
+        self.need_turn = True
+        self.load_from(asteroid)
+
+    def on_stop_at_mothership(self, mothership):
+        self.state_handle = self.state_handle.next_state
+        self.need_turn = True
+        self.unload_to(mothership)
 
     def _update_stats(self, point):
         """
