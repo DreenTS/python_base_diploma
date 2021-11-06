cf7535ee590462e8c0627d003a4b19b6d8b66a86
Юриков Василий <loney2ears@yandex.ru>
2021-11-06 20:39:05 +1000

    Updated yurikov.py file and UML class diagram

--- yurikov_team/yurikov.py
+++ yurikov_team/yurikov.py
@@ -1,7 +1,8 @@
-from astrobox.core import Drone
+from astrobox.core import Drone, Asteroid, MotherShip
 from robogame_engine.geometry import Point
 
 import yurikov_team.states as states
+from yurikov_team import utils
 from yurikov_team.log_config import configure_logger
 
 
@@ -17,15 +18,18 @@ class SpaceLogger:
 
     def log(self):
         res = 'Run stats:\n'
-        first_team, second_team = self.data['teams'].items()
-        res += f'{first_team[0]}: {first_team[1]}\n{second_team[0]}: {second_team[1]}\n'
-        if first_team[1] > second_team[1]:
-            res += f'\nWINNER: {first_team[0]}\n\n'
-        else:
-            res += f'\nWINNER: {second_team[0]}\n\n'
+        winner = list(self.data['teams'].keys())[0]
+        for name, score in self.data['teams'].items():
+            res += f'{name}: {score}\n'
+            if score > self.data['teams'][winner]:
+                winner = name
+
+        res += f'\nWINNER: {winner}\n\n'
+
         for k, v in self.data.items():
             if k != 'teams':
                 res += f'{k} = {v}\n'
+
         self.logger.info(res)
         print('\nLOGGING COMPLETED SUCCESSFULLY!\n')
 
@@ -33,6 +37,7 @@ class SpaceLogger:
 class YurikovDrone(Drone):
     """
     Класс дрона.
+    Если дронам разрешено стрелять - уничтожает всех соперников по карте.
     Собирает ресурс с астероидов и выгружает его на базе.
 
     """
@@ -41,28 +46,36 @@ class YurikovDrone(Drone):
         super().__init__(**kwargs)
         self.states_handle_list = None
         self.curr_state = None
+        self.prev_point = None
+        self.turret_point = None
+        self.in_regroup_move = True
+        self.enemy_drones = None
+        self.enemy_bases = None
         self.task = None
         self.is_transition_started = False
-        self.is_transition_finished = False
+        self.is_transition_finished = True
         self.target_to_turn = None
-        self.is_first_step = True
-        self.enemy_base = None
-        self.prev_point = None
-        self.manager = self
-        self.is_manager = True
-        self.need_log = True
         self.stats = None
-        temp_managers_list = [mate for mate in self.teammates if mate.is_manager]
-        if temp_managers_list:
-            self.manager = temp_managers_list[0]
+
+        _temp_managers_list = [mate for mate in self.teammates if mate.is_manager]
+        if _temp_managers_list:
+            self.manager = _temp_managers_list[0]
             self.is_manager = False
             self.need_log = False
+        else:
+            self.manager = self
+            self.is_manager = True
+            self.need_log = True
 
-    def on_heartbeat(self):
+    def on_heartbeat(self) -> None:
         """
         Выполняется при каждом шаге игры.
 
-        Проверяет, закончилась ли "миссия" дронов (можно ли логгировать).
+        #TODO Проверяет, закончилась ли "миссия" дронов (можно ли логгировать).
+
+        Есл текущее состояние дрона деактивировано:
+            ! меняет сстояние.
+
         Обновляет значение признаков оптимальности перемещения дрона.
 
         Вызывает метод состояния дрона on_heartbeat()
@@ -71,9 +84,15 @@ class YurikovDrone(Drone):
         :return: None
         """
 
-        if self.check_for_end():
-            if self.manager.need_log:
-                self.manager.log_stats()
+        # if self.scene.get_game_result()[0]:
+        #     if self.manager.need_log:
+        #         self.manager.log_stats()
+
+        if not self.curr_state.is_active:
+            self.switch_state()
+
+        if self.target is None:
+            self.target = self.curr_state.handle_action()
 
         self.update_stats(point=self.prev_point)
 
@@ -81,7 +100,7 @@ class YurikovDrone(Drone):
 
         self.curr_state.on_heartbeat()
 
-    def on_born(self):
+    def on_born(self) -> None:
         """
         Вызывается единожды при "рождении" дрона.
 
@@ -89,12 +108,14 @@ class YurikovDrone(Drone):
         то добавляет всем астероидам на сцене атрибут worker.
         В ходе игры распределение следующее: на 1 астероиде может работать 1 дрон.
 
+        Формирует два списка: с объектами вражеских дронов и вражеских баз.
+
         Устанавливает задачу для перемещения - "на загрузку".
-        Добавляет в список состояний два объекта классов состояний - "перемещение" и "перекачка ресурса".
+        Добавляет в список состояний объекты классов состояний (подробнее о классах см. docstrings классов).
         Вызывает метод переключения состояния switch_state()
         (подробнее см. docstrings метода).
 
-        Получает цель для перемещения из обработки внутри состояния и движется к ней.
+        Получает точку "турели" около своей базы и движется к ней.
 
         :return: None
         """
@@ -104,77 +125,118 @@ class YurikovDrone(Drone):
         if self.is_manager:
             for asteroid in self.asteroids:
                 asteroid.worker = None
-        if len(self.scene.motherships) > 1:
-            self.enemy_base = [m_ship for m_ship in self.scene.motherships if m_ship != self.my_mothership][0]
-        self.states_handle_list = [states.MoveState(self), states.TransitionState(self)]
+
         self.stats = {
             'empty_range': 0.0,
             'fully_range': 0.0,
             'not_fully_range': 0.0,
         }
-        self.task = states.LOAD_TASK
-        self.switch_state()
-        non_empty_asteroids = [astrd for astrd in self.asteroids if not astrd.is_empty]
-        self.target = self.curr_state.handle_action(asteroids_list=non_empty_asteroids, drone_free_space=self.free_space)
-        self.move_at(self.target)
 
-    def on_stop_at_asteroid(self, asteroid):
+        self.enemy_drones = [drone for drone in self.scene.drones if self.team != drone.team]
+        self.enemy_bases = [m_ship for m_ship in self.scene.motherships if m_ship != self.my_mothership]
+        self.task = states.LOAD_TASK
+        if len(self.scene.teams) <= 2 and self.is_manager:
+            self.states_handle_list = [states.MoveState(self), states.TransitionState(self)]
+        else:
+            self.states_handle_list = [states.TurretState(self), states.MoveState(self), states.TransitionState(self)]
+        self.switch_state()
+        self.turret_point = utils.get_turret_point(self, self.id)
+        self.move_at(self.turret_point)
+
+    def on_stop_at_point(self, target: Point) -> None:
+        """
+        При остановке у точки пространства.
+
+        Если текущее состояние - "турель":
+            ! устанавливает флаг "в процессе перегруппировки" в False.
+
+        :param target: Point, точка, у которой остановился дрон
+        :return: None
+        """
+
+        if isinstance(self.curr_state, states.TurretState):
+            self.in_regroup_move = False
+
+    def on_stop_at_asteroid(self, asteroid: Asteroid) -> None:
         """
         При остановке у астероида.
 
-        Переключает состояние.
-        Устанавливает флаг начала загрузки/выгрузки ресурса в значение True.
-        Начинает загрузку ресурса в трюм.
+        Если текущее состояние - "турель":
+            ! устанавливает флаг "в процессе перегруппировки" в False.
+        Иначе:
+            ! деактивирует текущее состояние;
+            ! устанавливает флаг начала загрузки/выгрузки ресурса в значение True;
+            ! начинает загрузку ресурса в трюм.
 
         :param asteroid: Asteroid object, объект астероида, у которого остановился дрон
         :return: None
         """
 
-        self.switch_state()
-        self.is_transition_started = True
-        self.load_from(asteroid)
+        if isinstance(self.curr_state, states.TurretState):
+            self.in_regroup_move = False
+        else:
+            self.switch_state()
+            self.is_transition_started = True
+            self.load_from(asteroid)
 
-    def on_stop_at_mothership(self, mothership):
+    def on_stop_at_mothership(self, mothership: MotherShip) -> None:
         """
         При остановке у базы.
 
-        Переключает состояние.
-        Устанавливает флаг начала загрузки/выгрузки ресурса в значение True.
-        Начинает выгрузку ресурса на базу.
+        Если текущее состояние - "турель":
+            ! движется к точки "турели".
+        Иначе:
+            ! деактивирует текущее состояние;
+            ! устанавливает флаг начала загрузки/выгрузки ресурса в значение True;
+
+            ! если объект базы - союзный:
+                начинает выгрузку ресурса на базу;
+            ! иначе:
+                начинает загрузку ресурса с базы.
 
         :param mothership: MotherShip object, объект базы
         :return: None
         """
 
-        self.switch_state()
-        self.is_transition_started = True
-        self.unload_to(mothership)
+        if isinstance(self.curr_state, states.TurretState):
+            self.move_at(self.turret_point)
+        else:
+            self.switch_state()
+            self.is_transition_started = True
+            if mothership == self.my_mothership:
+                self.unload_to(mothership)
+            else:
+                self.load_from(mothership)
 
-    def on_load_complete(self):
+    def on_load_complete(self) -> None:
         """
         По завершении загрузки ресурса.
 
-        Переключает состояние.
-        Устанавливает флаг начала загрузки/выгрузки ресурса в значение False.
-        Устанавливает флаг окончания загрузки/выгрузки ресурса в значение True.
-
+        Вызывает метод завершения "перекачки" ресурса.
 
         :return: None
         """
 
-        self.switch_state()
-        self.is_transition_started = False
-        self.is_transition_finished = True
+        self.on_transition_complete()
 
-    def on_unload_complete(self):
+    def on_unload_complete(self) -> None:
         """
         По завершении выгрузки ресурса.
 
-        Переключает состояние.
+        Вызывает метод завершения "перекачки" ресурса.
+
+        :return: None
+        """
+
+        self.on_transition_complete()
+
+    def on_transition_complete(self) -> None:
+        """
+        По завершении "перекачки" ресурса.
+
+        Сменяет текущее состояние.
         Устанавливает флаг начала загрузки/выгрузки ресурса в значение False.
-        Устанавливает флаг окончания загрузки/выгрузки ресурса в значение True.
-
-
+        Устанавливает флаг завершения загрузки/выгрузки ресурса в значение True.
         :return: None
         """
 
@@ -182,17 +244,18 @@ class YurikovDrone(Drone):
         self.is_transition_started = False
         self.is_transition_finished = True
 
-    def switch_state(self):
+    def switch_state(self) -> None:
         """
         Метод переключения состояния.
 
         Если текущее состояние не назначено (начало игры):
-            ! переключает на состояние "перемещение".
+            ! переключает на состояние "турель" и удаляет
+                данное состояние из списка состояний.
 
         Иначе:
-            ! диактивирует текущее состояние;
-            ! если переключает текущее состояние на следующее
-                ("перемещение" -> "перекачка ресурса" и наоборот).
+            ! если переключает текущее состояние на следующее:
+                "перекачка ресурса" или "турель" -> "перемещение";
+                "перемещение" -> "перекачка ресурса".
 
         Активирует текущее состояние.
 
@@ -201,15 +264,17 @@ class YurikovDrone(Drone):
 
         if self.curr_state is None:
             self.curr_state = self.states_handle_list[0]
+            if isinstance(self.curr_state, states.TurretState):
+                self.states_handle_list.pop(0)
         else:
             self.curr_state.is_active = False
-            if isinstance(self.curr_state, states.TransitionState):
+            if isinstance(self.curr_state, states.TurretState) or isinstance(self.curr_state, states.TransitionState):
                 self.curr_state = self.states_handle_list[0]
             else:
                 self.curr_state = self.states_handle_list[1]
         self.curr_state.is_active = True
 
-    def update_stats(self, point):
+    def update_stats(self, point: Point) -> None:
         """
         Вспомогательный метод. Вызывается при каждом шаге игры.
         Дополняет словарь менеджера по ключам, в зависимости от заполненности трюма,
@@ -226,22 +291,7 @@ class YurikovDrone(Drone):
         else:
             self.manager.stats['not_fully_range'] += self.distance_to(point)
 
-    def check_for_end(self):
-        """
-        Проверяет, закончилась ли "миссия" дронов (можно ли логгировать).
-        Формирует список всех дронов и астероидов.
-        Проверяет, являются ли все объекты списка пустыми
-        (отсутствует ли ресурс в трюмах дронов и недрах астероидов).
-
-        :return: bool, пустые ли все астероиды и все дроны
-        """
-
-        check_list = self.teammates + self.asteroids
-        is_empty_for_end = [obj.is_empty for obj in check_list]
-        is_empty_for_end.append(self.is_empty)
-        return all(is_empty_for_end)
-
-    def log_stats(self):
+    def log_stats(self) -> None:
         """
         Инициализация логгера.
 
@@ -259,5 +309,5 @@ class YurikovDrone(Drone):
             game_result['teams'][team_name] = team_resources
         game_result.update(self.stats)
         self.need_log = False
-        logger = SpaceLogger(data=game_result)
+        logger = SpaceLogger(game_result)
         logger.log()
